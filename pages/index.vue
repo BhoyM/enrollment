07f3dad7d185fa6bd2f50e6a41015e4f381a0935
BhoyMm <godwinjoshuacapalamendoza@gmail.comm>
2022-10-14 08:19:57 +0800

    OkayKaau

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,33 @@
 <template>
-  <Tutorial />
+        <div class="row">
+          <div class="col-lg-5 m-auto">
+            <div class="card mt-5 bg-dark">
+              <img class="rounded-sm w-55 h-55 border rounded-circle mx-auto d-block" src="avatar.png">
+              <div class="card-body">
+                <form>
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">
+                        <i class="fa fa-user fa-2x"></i>
+                      </span>
+                    </div>
+                    <input type="text" class="form-control py-4" placeholder="User Name">
+                  </div>
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">
+                        <i class="fa fa-lock fa-2x"></i>
+                      </span>
+                    </div>
+                    <input type="text" class="form-control py-4" placeholder="User Password">
+                  </div>
+                  <button class="btn btn-success">Login Now</button>
+                  <p class="float-right text-white mt-2"><input type="checkbox">Remember Me</p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
 </template>
 
 <script>
@@ -7,3 +35,5 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+
